328b52b3e9477f2a2571cf35bb0db90bc6ed5992
Ricardo Rodríguez <ricardo.rodriguez@externos-icex.es>
2020-01-21 19:07:04 +0100

    subida inicial

---
+++ SimpleProject/src/code/App.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package code;
+
+/**
+ * @author julen
+ *
+ */
+public abstract class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Buenas tardes");	
+
+	}
+
+}
